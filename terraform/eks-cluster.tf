d1d8dbeb1cf16e26deb0a9ba5291b35e2368ea0e
gitbex <bexruz@gmail.com>
2023-07-10 20:25:18 +0500

    version change

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
     source  = "terraform-aws-modules/eks/aws"
-    version = "~> 19.0"
+    version = "19.15.3"
     cluster_name = "myapp-eks-cluster"
     cluster_version = "1.24"
 
